0e175506bce726ef4011e31b1d52af5129f74a3a
Jinx <JeniceJinx@users.noreply.github.com>
2022-09-28 12:09:36 -0400

    added 4 components. Home, about, projects and Hobbies

--- CapstoneApp/src/app/app.module.ts
+++ CapstoneApp/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    HobbiesComponent
   ],
   imports: [
     BrowserModule
