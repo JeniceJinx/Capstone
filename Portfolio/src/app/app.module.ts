85057eae7a8c4c6fe3a68522e97bf73c48040d30
Jinx <JeniceJinx@users.noreply.github.com>
2022-10-02 14:45:50 -0400

    scrapped old page design.  this is a new set up with header and footer

---
+++ Portfolio/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { NavComponent } from './sharepage/nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactsComponent,
+    FooterComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
